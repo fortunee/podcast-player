ade8c1e3cf833fda189f1b645a055ed168e176fc
Fortune Ekeruo <12124618+fortunee@users.noreply.github.com>
2021-03-27 00:52:13 +0100

    move forward jump to the right and backward jump to the left

--- src/components/Player/Controls.js
+++ src/components/Player/Controls.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Controls = ({ disabled, isPlaying, jump, togglePlay }) => (
   <div className="player-controls">
-    <button className="btn" disabled={disabled} onClick={() => jump('forward')}>
-      &#x21bb;
+    <button
+      className="btn backward"
+      disabled={disabled}
+      onClick={() => jump('backward')}
+    >
+      &#x27F2;
     </button>
     <button className="btn playBtn" onClick={togglePlay} disabled={disabled}>
       {isPlaying ? <span>&#2405;</span> : <span>&#9658;</span>}
     </button>
-    <button
-      className="btn"
-      disabled={disabled}
-      onClick={() => jump('backward')}
-    >
-      &#x21ba;
+    <button className="btn forward" disabled={disabled} onClick={() => jump('forward')}>
+      &#x27F3;
     </button>
   </div>
 );
